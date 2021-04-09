43166a72bf05165814eb3cc1eff554faed260d9d
pawel.lyskawinski <pawel.lyskawinski@vao.pl>
2021-04-09 16:32:11 +0200

    randomFeature18

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Test_randomFeature01 from './components/Test_randomFeature01';
 import Test_randomFeature12 from './components/Test_randomFeature12';
 import Test_randomFeature16 from './components/Test_randomFeature16';
 import Test_randomFeature17 from './components/Test_randomFeature17';
+import Test_randomFeature18 from './components/Test_randomFeature18';
 
 export const App: React.FC = () => {
   return (
@@ -14,6 +15,7 @@ export const App: React.FC = () => {
       <Test_randomFeature12 />
       <Test_randomFeature16 />
       <Test_randomFeature17 />
+      <Test_randomFeature18 />
     </Box>
   );
 };
