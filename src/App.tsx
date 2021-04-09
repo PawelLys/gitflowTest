999ca93a8784f508a3800b1e5bac32feeb4418b6
pawel.lyskawinski <pawel.lyskawinski@vao.pl>
2021-04-09 15:55:41 +0200

    randomFeature01

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Box } from '@chakra-ui/react';
 import Test from './components/Test';
+import Test_randomFeature01 from './components/Test_randomFeature01';
 
 export const App: React.FC = () => {
   return (
     <Box background="#eaeaea">
       <Test />
+      <Test_randomFeature01 />
     </Box>
   );
 };
---
+++ src/components/Test_randomFeature01.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Box } from '@chakra-ui/react';
+
+export default () => {
+  return (
+    <Box background="#eaeaea" minH="100px">
+      RandomFeature01 added.
+    </Box>
+  );
+};
