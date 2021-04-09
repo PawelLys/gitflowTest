b266c23a9a3330ab9f6cf1666d860f0d80a062d6
pawel.lyskawinski <pawel.lyskawinski@vao.pl>
2021-04-09 16:18:33 +0200

    randomFeature_16

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Box } from '@chakra-ui/react';
 import Test from './components/Test';
 import Test_randomFeature01 from './components/Test_randomFeature01';
 import Test_randomFeature12 from './components/Test_randomFeature12';
+import Test_randomFeature16 from './components/Test_randomFeature16';
 
 export const App: React.FC = () => {
   return (
@@ -10,6 +11,7 @@ export const App: React.FC = () => {
       <Test />
       <Test_randomFeature01 />
       <Test_randomFeature12 />
+      <Test_randomFeature16 />
     </Box>
   );
 };
