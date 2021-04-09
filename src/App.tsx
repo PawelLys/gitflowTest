df0577618db421ad7403674c8f018900279582c6
pawel.lyskawinski <pawel.lyskawinski@vao.pl>
2021-04-09 15:26:32 +0200

    master commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Box } from '@chakra-ui/react';
+import Test from './components/Test';
+
+export const App: React.FC = () => {
+  return (
+    <Box background="#eaeaea">
+      <Test />
+    </Box>
+  );
+};
